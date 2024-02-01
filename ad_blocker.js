8407e9141ddec5c3097c11e0abe44b5415295491
PX Zone <158117360+pxzone@users.noreply.github.com>
2024-02-01 16:34:51 +0800

    Update ad_blocker.js

--- ad_blocker.js
+++ ad_blocker.js
@@ -7,59 +7,44 @@
 
 
 (function() {
- let url = location.href;
-  // check if page is profile page
-  if(url.indexOf("index.php?action=profile") > 0){
-  // removing top banner ad
-    var first_link = document.querySelector("#main_content_section center a:first-child");
-    if (first_link) {
-      first_link.setAttribute("hidden", "hidden");
+    let url = location.href;
+     // check if page is profile page
+     if(url.indexOf("index.php?action=profile") > 0){
+        blockTopbanner();
     }
-    document.querySelector("#main_content_section center br");
-    var small_element = document.querySelectorAll("#main_content_section center small");
-    small_element.forEach(function(small) {
-   small.setAttribute("hidden", "hidden");
-    });
- }
-  // check if page is in topic page
-  else if( url.indexOf("index.php?topic") > 0){
-    
-    // removing top banner ad
-    var first_link = document.querySelector("#main_content_section center a:first-child");
-    if (first_link) {
-      first_link.setAttribute("hidden", "hidden");
+     // check if page is in topic page
+     else if( url.indexOf("index.php?topic") > 0){
+       blockTopbanner();
+       blockThreadAd();
+       blockForumRulesImg();
     }
-    document.querySelector("#main_content_section center br");
-    var small_element = document.querySelectorAll("#main_content_section center small");
-    small_element.forEach(function(small) {
-   small.setAttribute("hidden", "hidden");
-    });
-    
-    //removing banner ad in every thread
-    var ad_div = document.querySelectorAll('div[class*="windowbg"]');
-    if (ad_div.length > 1) {
-        ad_div[1][removed].removeChild(ad_div[1]);
+     // check if page is home page, boards and other forum pages
+     else if(url.indexOf("index.php") > 0){
+       blockTopbanner();
+     }
+   
+    function blockTopbanner(){
+        var first_link = document.querySelector("#main_content_section center a:first-child");
+        if (first_link) {
+            first_link.setAttribute("hidden", "hidden");
+        }
+        document.querySelector("#main_content_section center br");
+        var small_element = document.querySelectorAll("#main_content_section center small");
+        small_element.forEach(function(small) {
+            small.setAttribute("hidden", "hidden");
+        });
     }
-    
-    // removing repeated forum images in every thread
-    var forum_rules_img = document.querySelectorAll('img[src="https://i.imgur.com/CtEASyB.png"]');
-    forum_rules_img.forEach(function(img) {
-        img[removed].removeChild(img);
-    });
- }
-  // check if page is home page, boards and other forum pages
-  else if(url.indexOf("index.php") > 0){
-    // removing top banner ad
-    var first_link = document.querySelector("#main_content_section center a:first-child");
-    if (first_link) {
-      first_link.setAttribute("hidden", "hidden");
+    function blockThreadAd(){
+        var ad_div = document.querySelectorAll('div[class*="windowbg"]');
+        if (ad_div.length > 1) {
+            ad_div[1].parentNode.removeChild(ad_div[1]);
+        }
     }
-    document.querySelector("#main_content_section center br");
-    var small_element = document.querySelectorAll("#main_content_section center small");
-    small_element.forEach(function(small) {
-   small.setAttribute("hidden", "hidden");
-    });
-  }
-  
-  
+    function blockForumRulesImg(){
+        var forum_rules_img = document.querySelectorAll('img[src="https://i.imgur.com/CtEASyB.png"]');
+        forum_rules_img.forEach(function(img) {
+            img.parentNode.removeChild(img);
+        });
+    }
 })();
+  
